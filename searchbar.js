c70faf0497be4dc70b948503d9110556dd3fb593
TAGSC <42639125+TAGSC@users.noreply.github.com>
2023-12-31 18:28:58 +0800

    Add files via upload

---
+++ searchbar.js
@@ -0,0 +1,20 @@
+function performSearch() {
+    const searchTerm = document.getElementById('searchInput').value;
+    alert("Searching for... " + searchTerm)
+    
+}
+
+function addToCart() {
+    alert("Added to Cart!")
+    
+}
+
+function purchased() {
+    alert("Your purchases will soon be delivered in your location!")
+    
+}
+
+function removed() {
+    alert("Item removed!")
+    
+}
